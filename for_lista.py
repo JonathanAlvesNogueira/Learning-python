c6bd4a3d841205cb88cda365723f430efc69802b
Jonathan <joth10@live.com>
2023-04-30 01:01:41 -0300

    Aprendendo indice e for em python

---
+++ for_lista.py
@@ -0,0 +1,5 @@
+lista = ['maria', 'joao', 'pedro']
+tamanho = range(len(lista))
+
+for indice in tamanho :
+    print(f' esse aqui é o indice {indice} nome: {lista[indice]} esse aqui é o tipo : {type(lista[indice])}')
