4b60785b99da4411e1db3fedc3288d28dd4ce32c
Jonathan <joth10@live.com>
2023-04-30 22:39:23 -0300

    adicionando parte 2

--- manipulando_dictory.py
+++ manipulando_dictory.py
@@ -12,7 +12,13 @@ print(pessoa[sobrenome])
 ##deleta chave valor
 del pessoa[sobrenome]
 
-
+if pessoa.get('nome') is not None:
+    print('pessoa existe')
+    pessoa1 = pessoa['nome']
+    print(f'essa é a pessoa {pessoa1}' )
+else:
+    print('pessoa nao existe')
+    
 
 ##Imprime pessoa restantes
 print(pessoa)
