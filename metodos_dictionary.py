69b2088804689bf6da9ceecd07cff6ae295696ca
Jonathan <joth10@live.com>
2023-04-30 23:05:15 -0300

    Aprendendo pop dictionary

--- metodos_dictionary.py
+++ metodos_dictionary.py
@@ -14,3 +14,27 @@ novaPessoa = pessoa.items()
 print(novaPessoa)
 for chave, valor in novaPessoa:
     print(chave, valor)
+
+### setdefault = set chave e valor se a chave nao existir ou nao tiver um valor
+pessoa.setdefault('idade', 100)
+print(pessoa['idade'])
+
+###copy - shallow copy
+copia = {
+    'name' : 'jonathan',
+    'empresa': 'gerdau',
+    'lista' : [0,1,2] 
+}
+
+copia2 = copia.copy()
+print(copia)
+print(copia2)
+
+
+###Usando get
+print(pessoa.get('valor', 'nao existe'))
+
+
+### Pop
+nome = pessoa.pop('nome')
+print(nome)
