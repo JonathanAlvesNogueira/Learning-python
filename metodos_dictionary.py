a7705c6d0c26f6590de235f4a42bf373668fbab1
Jonathan <joth10@live.com>
2023-04-30 22:45:11 -0300

    Aprendendo a usar items

--- metodos_dictionary.py
+++ metodos_dictionary.py
@@ -5,3 +5,12 @@ pessoa = {
 }
 ### pega as chaves
 print(pessoa.keys())
+
+### pega values
+print(pessoa.values())
+
+### Usando items = pegar chave e valor
+novaPessoa = pessoa.items()
+print(novaPessoa)
+for chave, valor in novaPessoa:
+    print(chave, valor)
