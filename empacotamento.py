9bc02f299521642f31a86bf58cdb5af3b92d53af
Jonathan <joth10@live.com>
2023-05-06 04:02:04 -0300

    Empacotamento

---
+++ empacotamento.py
@@ -0,0 +1,6 @@
+tipo = {
+    'nome' : 'jonathan',
+    'mapa' : 'sao paulo',
+}
+
+print(type(tipo))
--- lambdas.py
+++ lambdas.py
@@ -1,2 +1,18 @@
 lista = [31,19,11,8,6,1]
 print(lista.sort())
+
+lista2 = [
+    {'nome' : 'jonathan', 'sobrenome' : 'alves'},
+    {'nome' : 'alex', 'sobrenome': 'cleber'},
+    {'nome' : 'douglas', 'sobrenome' : 'teves'}
+]
+print(lista2)
+
+print()
+
+
+def ordena(item):
+    return item['nome']
+
+lista2.sort(key=ordena)
+print(lista2)
