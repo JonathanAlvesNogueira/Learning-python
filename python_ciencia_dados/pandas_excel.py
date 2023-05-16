ef15e12efcc24c8d7555409b7712401fa8fae371
Jonathan <joth10@live.com>
2023-05-16 04:34:16 -0300

    Adicionando todos os arquivos na mesma variavel

--- python_ciencia_dados/pandas_excel.py
+++ python_ciencia_dados/pandas_excel.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
+
 ### lendo arquivo
-arq = pd.read_excel('dadospandas.xlsx')
+arq = pd.read_excel('C:\\Users\\joth1\\Desktop\\python\\Aprendendo-python\\python_ciencia_dados\\dadospandas.xlsx')
+arq1 = pd.read_excel('C:\\Users\\joth1\\Desktop\\python\\Aprendendo-python\\python_ciencia_dados\\dadospandas copy.xlsx')
+arq2 = pd.read_excel('C:\\Users\\joth1\\Desktop\\python\\Aprendendo-python\\python_ciencia_dados\\dadospandas copy 2.xlsx')
+
+# juntando arquivos 
+arq_final= pd.concat([arq,arq1,arq2])
+
+print(arq_final.head(30))
