0905888e42f977e5decf889bbfab7e4261af20d3
Jonathan <joth10@live.com>
2023-07-16 20:33:21 -0300

    lista com elementos iteraveis

---
+++ estrutura_dados/lista.py
@@ -0,0 +1,11 @@
+### Cria uma lista com  a string sendo a saida ['p', 'y', 't', 'h', 'o', 'n'] poi string é um elemento iteravel 
+letras = list("Python")
+print(letras)
+
+### Cria uma lista de 10 elementos sendo [0,1,2,3,4,5,6,7,8,9]
+numeros = list(range(10))
+print(numeros)
+
+
+
+
