f012f8246e25e2cde89967e288a8688e4dd3eead
Jonathan <joth10@live.com>
2023-07-16 23:10:54 -0300

    puxando registros do dict

--- estrutura_dados/dict.py
+++ estrutura_dados/dict.py
@@ -10,5 +10,13 @@ pessoa2 = dict(nome='jonathan', idade=19)
 print(f'forma 2 de declarar : {pessoa2}'); print()
 
 pessoa['cidade'] = 'São Paulo'
-print(f' ADICIONANDO MAIS UM ELEMENTO CHAVE-VALOR {pessoa}') 
+print(f' ADICIONANDO MAIS UM ELEMENTO CHAVE-VALOR {pessoa}') ; print()
+
+
+### puxando valor do dict 
+print('informações usuarios: ')
+print(pessoa['nome'])
+print(pessoa['idade'])
+print(pessoa['cidade']); print()
+
 
