00b2fd3210264d8e40404f335e09dcc7768edff2
Jonathan <joth10@live.com>
2023-07-16 22:52:21 -0300

    Declarando dictonary

---
+++ estrutura_dados/dict.py
@@ -0,0 +1,14 @@
+### FORMA UM DE DECLARAR UM DICT 
+pessoa = {
+    'nome' : 'jonathan', 
+    'idade' : 19
+}
+print( f'forma 1 de declarar  {pessoa}'); print()
+
+### FORMA 2 DE DECLARAR UM DICT 
+pessoa2 = dict(nome='jonathan', idade=19)
+print(f'forma 2 de declarar : {pessoa2}'); print()
+
+pessoa['cidade'] = 'São Paulo'
+print(f' ADICIONANDO MAIS UM ELEMENTO CHAVE-VALOR {pessoa}') 
+
