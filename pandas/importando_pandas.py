e2b54d62ed6dbef8eeca884ee46c1bee8b76be8e
Jonathan <joth10@live.com>
2023-07-23 11:05:38 -0300

    importando pandas

--- pandas/importando_pandas.py
+++ pandas/importando_pandas.py
@@ -1,4 +1,13 @@
 import pandas as pd
-url = 'C://Users//joth1//Downloads//Calculos_Jonathan.xlsx'
-df = pd.read_excel(url)
-df.head()
+
+# Substitua 'dados.xlsx' pelo caminho completo do seu arquivo Excel, usando barras invertidas.
+caminho_arquivo = 'C:\\Users\\joth1\\Downloads\\dados.xlsx'  # Usando barras invertidas
+
+# Substituir as barras invertidas por barras normais
+caminho_arquivo = caminho_arquivo.replace("\\", "/")
+
+# Carregar o arquivo Excel em um DataFrame
+df = pd.read_excel(caminho_arquivo)
+
+# Exibir as 5 primeiras linhas do DataFrame
+print(df.head(5))
