d3cc543d52a0cf49f064c5aa27c671a4bf5d15db
Jonathan <joth10@live.com>
2023-07-23 16:08:52 -0300

    Adicionando conexao com sql e select

--- conexao_bancodados/banco_para_excel.py
+++ conexao_bancodados/banco_para_excel.py
@@ -10,5 +10,17 @@ conexao1  = conexao.connect(
     database="exemplo"
 )
 
+cursor = conexao1.cursor()
+
+use_banco = "USE EXEMPLO"
+cursor.execute(use_banco)
+
+selecao_dados = "SELECT * FROM PRODUTO"
+cursor.execute(selecao_dados)
+resultado = cursor.fetchall()
+
+for linha in resultado:
+    print(linha)
+
 cursor.close()
-conexao.close()
+conexao1.close()
